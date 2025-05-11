6abffa3d17a05e41eba7804a4b4454a12178fb13
pratya <pratya@cardx.co.th>
2025-05-11 19:56:54 +0700

    refactor code

--- src/main/java/com/tirmizee/config/WebClientConfig.java
+++ src/main/java/com/tirmizee/config/WebClientConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import static com.google.common.net.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @Configuration
 public class WebClientConfig {
 
@@ -11,7 +14,7 @@ public class WebClientConfig {
     public WebClient dialogflowWebClient() throws Exception {
         return WebClient.builder()
                 .baseUrl("https://asia-southeast1-dialogflow.googleapis.com")
-                .defaultHeader("Content-Type", "application/json")
+                .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
                 .build();
     }
 
